937a47ffcdf01da02e00dc2cf860c0930f6a3032
Andrew <98215048+eee-Andrew@users.noreply.github.com>
2024-10-29 23:33:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+#This library comes with the models v8 nano /large /XL https://docs.ultralytics.com/usage/python/
+from ultralytics import YOLO
 
+if __name__ == "__main__":
+    # Initialize the YOLO model with a pre-trained model, like the YOLOv8 nano model
+    
+    model = YOLO("yolov8n.yaml")  # use yolov8n.pt, yolov8s.pt, etc., depending on your needs
+    # Train the model with your dataset
+    # When training a model using frameworks like YOLOv7 or YOLOv8, you can specify various parameters, 
+    #including epochs and batch.  
+  
+    results = model.train(data="data.yaml", epochs=100)
